4a8e1596e0bd62c02890e25491ad9f472cc75ea1
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-02-11 14:12:07 -0800

    Added new file, CountryList.java, to hold my ArrayList

---
+++ src/main/java/com/lambdaschool/countries/CountryList.java
@@ -0,0 +1,20 @@
+package com.lambdaschool.countries;
+
+import java.util.ArrayList;
+
+// Create an array list for country objects
+//        * Each country object will contain
+//        * a country name, population, land mass size, and median age
+//        * should contain the standard getter and setters
+
+public class CountryList
+{
+
+    // my ArrayList of countries
+    public ArrayList<Country> countryList = new ArrayList<Country>();
+
+    public CountryList()
+        countryList.add(new Country("China",1420062022,9388211,39));
+        countryList.add(new Country("India",1368737513,2973190,28));
+        countryList.add(new Country("U.S.",329093110,9147420,38));
+}
