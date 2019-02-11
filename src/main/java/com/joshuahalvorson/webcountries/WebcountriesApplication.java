93c34c1dea86cdd7c9dabafc7e6eb8860a42e6b9
Josh Halvorson <Jhalvorson5@mail.greenriver.edu>
2019-02-11 11:41:56 -0800

    Initial commit

---
+++ src/main/java/com/joshuahalvorson/webcountries/WebcountriesApplication.java
@@ -0,0 +1,14 @@
+package com.joshuahalvorson.webcountries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebcountriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebcountriesApplication.class, args);
+    }
+
+}
+
