bcd691fce62da7382e80d3e1b0b7b84f3735b718
Eric <eric_gomez_1@hotmail.com>
2019-02-11 11:21:52 -0800

    boilerplate created

---
+++ src/main/java/app/webcountries/WebCountriesApplication.java
@@ -0,0 +1,16 @@
+package app.webcountries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCountriesApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(WebCountriesApplication.class, args);
+    }
+
+}
+
