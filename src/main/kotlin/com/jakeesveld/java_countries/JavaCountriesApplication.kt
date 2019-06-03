159e3078201f2d3c65fdf5d17ee92a4d2ad3616e
Jake Esveld <thejakeesveld@gmail.com>
2019-06-03 11:07:58 -0700

    Initial commit

---
+++ src/main/kotlin/com/jakeesveld/java_countries/JavaCountriesApplication.kt
@@ -0,0 +1,11 @@
+package com.jakeesveld.java_countries
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JavaCountriesApplication
+
+fun main(args: Array<String>) {
+    runApplication<JavaCountriesApplication>(*args)
+}
