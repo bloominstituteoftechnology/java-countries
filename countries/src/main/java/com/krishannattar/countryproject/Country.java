e5b720cb90bf462bed0979b55feb5cfabc052d00
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-30 18:16:08 -0700

    added Employee class

---
+++ countries/src/main/java/com/krishannattar/countryproject/Country.java
@@ -0,0 +1,58 @@
+package com.krishannattar.countryproject;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Country {
+
+    private static final AtomicLong country = new AtomicLong();
+    private long id;
+    private String name;
+    private int population;
+    private int landMassSize;
+    private int medianAge;
+
+    public Country(String name, int population, int landMassSize, int medianAge)
+    {
+        this.id = country.incrementAndGet();
+        this.name = name;
+        this.population = population;
+        this.landMassSize = landMassSize;
+        this.medianAge = medianAge;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public int getLandMassSize() {
+        return landMassSize;
+    }
+
+    public void setLandMassSize(int landMassSize) {
+        this.landMassSize = landMassSize;
+    }
+
+    public int getMedianAge() {
+        return medianAge;
+    }
+
+    public void setMedianAge(int medianAge) {
+        this.medianAge = medianAge;
+    }
+}
---
+++ countries/src/main/java/com/krishannattar/countryproject/CountryController.java
@@ -0,0 +1,4 @@
+package com.krishannattar.countryproject;
+
+public class CountryController {
+}
