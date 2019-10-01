f50f2b02c5a3c958742864de834e471e25220d3d
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-30 17:52:32 -0700

    project initiated

---
+++ countries/src/main/java/com/krishannattar/countryproject/CountryprojectApplication.java
@@ -0,0 +1,13 @@
+package com.krishannattar.countryproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountryprojectApplication.class, args);
+    }
+
+}
