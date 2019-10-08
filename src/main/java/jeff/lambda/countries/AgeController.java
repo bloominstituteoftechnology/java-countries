5912e19c37833a8c7e892ba2ca2bd469204cf13a
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-10-07 18:40:18 -0700

    finished mvp

---
+++ src/main/java/jeff/lambda/countries/AgeController.java
@@ -0,0 +1,42 @@
+package jeff.lambda.countries;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/age")
+public class AgeController
+{
+    // /age/{age}
+    @GetMapping(value ="/{age}", produces ={"application/json"})
+    public ResponseEntity<?> getCountryByMedianAgeOrGreater(@PathVariable int age)
+    {
+        ArrayList<Country> rtnCountries = CountriesApplication.ourCountryList.findCountries(c -> c.getMedianAge() >= age);
+        return new ResponseEntity<>(rtnCountries, HttpStatus.OK);
+    }
+
+    // /age/max
+    @GetMapping(value = "/max", produces = {"application/json"})
+    public ResponseEntity<?> getCountryByHighestMedianAge()
+    {
+        ArrayList<Country> rtnCountries = CountriesApplication.ourCountryList.countryList;
+        rtnCountries.sort((c1, c2) -> (c1.getMedianAge() - c2.getMedianAge()));
+        return new ResponseEntity<>(rtnCountries.get(rtnCountries.size() - 1), HttpStatus.OK);
+    }
+
+    // /age/min
+    @GetMapping(value = "/min", produces = {"application/json"})
+    public ResponseEntity<?> getCountryByLowestMedianAge()
+    {
+        ArrayList<Country> rtnCountries = CountriesApplication.ourCountryList.countryList;
+        rtnCountries.sort((c1, c2) -> (c1.getMedianAge() - c2.getMedianAge()));
+        return new ResponseEntity<>(rtnCountries.get(0), HttpStatus.OK);
+    }
+
+}
