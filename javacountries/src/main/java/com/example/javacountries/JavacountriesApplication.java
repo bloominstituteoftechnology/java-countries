27439d723c1a07f560ec965561b70626640a56f4
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2019-11-04 14:11:23 -0700

    JavaCountries
    Albert Yakubov -Java Countries
    Initial Commit

---
+++ javacountries/src/main/java/com/example/javacountries/JavacountriesApplication.java
@@ -0,0 +1,13 @@
+package com.example.javacountries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacountriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavacountriesApplication.class, args);
+    }
+
+}
---
+++ javacountries/src/test/java/com/example/javacountries/JavacountriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javacountries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacountriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
