31a68800a66432fb15522fd9e4af2d4f56656f8b
cladams0203 <cladams0203@gmail.com>
2020-08-27 10:43:22 -0400

    finished population total

--- src/main/java/com/lambdaschool/countries/controllers/CountryController.java
+++ src/main/java/com/lambdaschool/countries/controllers/CountryController.java
@@ -1,6 +1,9 @@
 package com.lambdaschool.countries.controllers;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.lambdaschool.countries.models.Country;
 import com.lambdaschool.countries.repositories.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,6 +47,22 @@ public class CountryController {
         countryRepository.findAll().iterator().forEachRemaining(countryList::add);
         List<Country> rtnList = findCountries(countryList, e -> e.getName().charAt(0) == letter);
         return new ResponseEntity<>(rtnList, HttpStatus.OK);
+    }
+    //http://localhost:2019/population/total
+    @GetMapping(value = "/population/total", produces = {"application/json"})
+    public ResponseEntity<?> TotalPopulation(){
+        List<Country> countryList = new ArrayList<>();
+        countryRepository.findAll().iterator().forEachRemaining(countryList::add);
+
+        long totalPopulation = 0;
+        for (Country c : countryList){
+            totalPopulation = totalPopulation + c.getPopulation();
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode total = mapper.createObjectNode();
+        total.put("population_total", totalPopulation);
+
+        return new ResponseEntity<>(total, HttpStatus.OK);
     }
 
 
