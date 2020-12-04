10238fde43605abaf9e2c16d9ce9525397a84f36
Simone Ballard <simone.a.ballard@gmail.com>
2020-12-03 21:58:31 -0700

    all changes, but accidentally deleted the data.sql file too

---
+++ countries/src/main/java/com/lambdaschool/countries/repositories/CountryRepository.java
@@ -0,0 +1,8 @@
+package com.lambdaschool.countries.repositories;
+
+import com.lambdaschool.countries.models.Country;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CountryRepository extends CrudRepository<Country, Long>
+{
+}
---
+++ countries/src/test/java/com/lambdaschool/countries/CountriesApplicationTests.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.countries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
