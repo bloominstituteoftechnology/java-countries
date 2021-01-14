3be0bea0cb55eff1b9a62cfd43d0ee35e627d194
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-01-14 18:49:41 -0500

    http://localhost:2019/names/start/u

--- src/main/java/com/lambadaschool/countries/controllers/CountriesController.java
+++ src/main/java/com/lambadaschool/countries/controllers/CountriesController.java
@@ -1,11 +1,13 @@
 package com.lambadaschool.countries.controllers;
 
+import com.lambadaschool.countries.CheckCountry;
 import com.lambadaschool.countries.models.Country;
 import com.lambadaschool.countries.repositories.CountriesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -26,5 +28,31 @@ public class CountriesController {
         myList.sort((firstItem, secondItem) -> (firstItem.getName().charAt(0) > secondItem.getName().charAt(0)) ? 1 : -1);
 
         return new ResponseEntity<>(myList, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/names/start/{letter}", produces = "application/json")
+    public ResponseEntity<?> findByName(@PathVariable char letter) {
+        List<Country> myList = new ArrayList<>();
+
+        char UpperCaseLetter = Character.toUpperCase(letter);
+
+        countriesRepository.findAll().iterator().forEachRemaining(myList::add);
+
+        List<Country> filteredList = filterCountries(myList, (c) -> c.getName().charAt(0) == UpperCaseLetter);
+
+        return new ResponseEntity<>(filteredList, HttpStatus.OK);
+
+    }
+
+    private List<Country> filterCountries(List<Country> countriesList, CheckCountry tester) {
+        List<Country> rtnList = new ArrayList<>();
+
+        for (Country c : countriesList) {
+            if (tester.test(c)) {
+                rtnList.add(c);
+            }
+        }
+
+        return  rtnList;
     }
 }
